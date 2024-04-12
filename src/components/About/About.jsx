92262ac00af89fa87504de98810e9ad50915bee5
lons <lonsnw@gmail.com>
2024-04-11 20:32:15 -0500

    Adding with most functionality

---
+++ src/components/About/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+
+function About() { 
+    return (
+        <p> This is some very important information about these students.  Have you seen how important this is?
+        </p>
+    )
+}
+
+export default About;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,19 +1,31 @@
 import axios from 'axios';
 import './App.css';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import StudentForm from '../StudentForm/StudentForm.jsx';
 import StudentList from '../StudentList/StudentList.jsx';
+import About from '../About/About.jsx';
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-            <h1 className="App-title">GitHub Student List</h1>
-            </header>
-            <br/>
-            <StudentForm />
-
-            <p>Student list:</p>
-            <StudentList />
+            <Router>
+                <header className="App-header">
+                    <h1 className="App-title">GitHub Student List</h1>
+                    <nav> 
+                        <p><Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/allStudents">Students</Link></p>
+                    </nav>
+                </header>
+                <br/>
+                <Route exact path ="/">
+                <StudentForm />
+                </Route>
+                <Route exact path="/about">
+                <About /> 
+                </Route>
+                <Route path="/allStudents">
+                <StudentList />
+                </Route>
+            </Router>
         </div>
     );
 }
